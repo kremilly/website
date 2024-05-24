b5c78712b15f33e3322fcb2e44c704393f63b0cb
Kremilly <contact@kremilly.com>
2024-05-24 11:10:49 -0300

    feat: added option for sponsor me

--- js/apis/github.js
+++ js/apis/github.js
@@ -14,7 +14,7 @@ async function github() {
         Attr.set("#user-info-blog", "href", response.blog)
         Attr.set("#user-info-github", "href", response.html_url)
         Attr.set("#pinned-repos-button", "href", response.html_url)
-        Attr.set("#user-info-twitter", "href", "https://twitter.com/" + response.twitter_username)
+        Attr.set("#user-info-sponsor", "href", "https://github.com/sponsors/" + response.login)
     });
 };
 
